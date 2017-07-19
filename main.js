1183a96256fd06948428b590d80a98dc2ed27854
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-19 09:59:16 -0400

    cleaned up

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 let container = document.querySelector('.container');
 let input = document.querySelector('#input');
 let button = document.querySelector('.button')
-// let value = input.setAttribute("value", input.value)
 
 button.addEventListener("click", function click() {
 
-fetch(
-    "https://crossorigin.me/http://www.recipepuppy.com/api/?i=" + input.value
-  )
-  .then(function(response) {
-    if (response.status !== 200) {
-      console.log(response.status);
-      return;
-    }
-    response.json().then(function(response) {
-        let template = '';
-        response.results.forEach(function(result) {
-          template += `
+  fetch(
+      "https://crossorigin.me/http://www.recipepuppy.com/api/?q=" + input.value
+    )
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(response) {
+          let template = '';
+          response.results.forEach(function(result) {
+            template += `
           <div class="results">
             <img src="${result.thumbnail}">
             <h3><a href="${result.href}" target="blank">${result.title}</a></h3>
           </div>
       `;
-          container.innerHTML = template
+            container.innerHTML = template
+          });
+        })
+        .catch(function(err) {
+          console.log("Fetch Error :-S", err);
         });
-      })
-      .catch(function(err) {
-        console.log("Fetch Error :-S", err);
-      });
-  });
+    });
 });
